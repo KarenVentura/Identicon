6adc7022c7e66c8ecef27970b91ddcec5853d2f1
Karen Ventura <akv802@gmail.com>
2020-06-15 15:50:53 -0500

    Generate identicon image with erlang library

--- lib/image.ex
+++ lib/image.ex
@@ -1,4 +1,4 @@
 defmodule Identicon.Image do
   # we should define the struct property that we are going to use
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
